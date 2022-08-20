54256fb10bea808527f32b9b1f6b26dc1685a351
Omkar Birajdar <ogbirajdar@gmail.com>
2022-08-20 12:06:02 +0530

    added third.cpp

---
+++ third.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+int main()
+{
+    ll t;
+    cin>>t;
+    while(t--)
+    {
+        cout<< "Hello OM !\n";
+    }
+return 0;
+}
